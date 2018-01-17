aaad6be5c97319076b5030488885fbe0c44226f8
MaddeeRubenson <rubenson.m@gmail.comgit config --global user.email rubenson.m@gmail.comgit config --global user.email rubenson.m@gmail.com>
2018-01-16 22:19:04 -0800

    updates

--- Functions/mthoodfun.R
+++ Functions/mthoodfun.R
@@ -1,38 +1,38 @@
-### Mt Hood Snow Depth Plot
-#this function creates a plot of the snow depth at Meadows, Skibowl, and Timberline
-#inputs are start date and end date (%y-%m-%d), default set to present date and 15 days prior
+### Mt Hood Snow Plots
 
 SnowDepthPlot <- function(startdate = (Sys.Date() - 15), enddate = Sys.Date()) {
-
-#https://www.nwac.us/data-portal/location/mt-hood/q?field_name=snow_depth&year=2017&custom_startdate=2017-12-17&custom_enddate=2017-12-19
-require(ggplot2)
-require(data.table)
-require(curl)
-
-url <- paste0('https://www.nwac.us/data-portal/csv/location/mt-hood/sensortype/snow_depth/start-date/', startdate, '/', 'end-date/', enddate, '/')  
-mthood <- fread(url)
+  #this function creates a plot of the snow depth at Meadows, Skibowl, and Timberline
+  #inputs are start date and end date (%y-%m-%d), default set to present date and 15 days prior
   
-#mthood <- fread('https://www.nwac.us/data-portal/csv/location/mt-hood/sensortype/snow_depth/start-date/2017-12-17/end-date/2017-12-20/')
-
-names(mthood) <- c('DateTimePST', 'SkiBowl', 'MtHoodMeadows', 'TimberlineLodge')
-mthood$DateTimePST <- as.POSIXct(mthood$DateTimePST)
-mthood <- na.omit(mthood)
-
-g <- ggplot(data = mthood, aes()) +
-  geom_point(aes(x = DateTimePST, y = SkiBowl, color = 'Ski Bowl Summit (5010 feet)')) +
-  geom_point(aes(x = DateTimePST, y = MtHoodMeadows, color = 'Meadows Base(5380 feet)')) +
-  geom_point(aes(x = DateTimePST, y = TimberlineLodge, color = 'Timberline Lodge (5880 feet)')) +
-  ylab('Snow Depth (inches)') +
-  xlab('Date/Time (PST)') + 
-  theme_bw()+
-  theme(legend.position = "top", legend.direction = "horizontal") +
-  labs(colour = "") 
-
-return(g)
-
+  #https://www.nwac.us/data-portal/location/mt-hood/q?field_name=snow_depth&year=2017&custom_startdate=2017-12-17&custom_enddate=2017-12-19
+  require(ggplot2)
+  require(data.table)
+  require(curl)
+  
+  url <- paste0('https://www.nwac.us/data-portal/csv/location/mt-hood/sensortype/snow_depth/start-date/', startdate, '/', 'end-date/', enddate, '/')  
+  mthood <- fread(url)
+  
+  #mthood <- fread('https://www.nwac.us/data-portal/csv/location/mt-hood/sensortype/snow_depth/start-date/2017-12-17/end-date/2017-12-20/')
+  
+  names(mthood) <- c('DateTimePST', 'SkiBowl', 'MtHoodMeadows', 'TimberlineLodge')
+  mthood$DateTimePST <- as.POSIXct(mthood$DateTimePST)
+  mthood <- na.omit(mthood)
+  
+  g <- ggplot(data = mthood, aes()) +
+    geom_line(aes(x = DateTimePST, y = SkiBowl, color = 'Ski Bowl Summit (5010 feet)')) +
+    geom_line(aes(x = DateTimePST, y = MtHoodMeadows, color = 'Meadows Base(5380 feet)')) +
+    geom_line(aes(x = DateTimePST, y = TimberlineLodge, color = 'Timberline Lodge (5880 feet)')) +
+    ylab('Snow Depth (inches)') +
+    xlab('Date/Time (PST)') + 
+    theme_bw()+
+    theme(legend.position = "top", legend.direction = "horizontal") +
+    labs(colour = "") 
+  
+  return(g)
+  
 }
 
-#plot <- SnowDepthPlot()
+SnowDepthPlot()
 
 snowfall <- function(startdate = (Sys.Date() - 5), enddate = Sys.Date()) {
   url <- paste0('https://www.nwac.us/data-portal/csv/location/mt-hood/sensortype/snowfall_24_hour/start-date/', startdate, '/', 'end-date/', enddate, '/')  
@@ -47,15 +47,15 @@ snowfall <- function(startdate = (Sys.Date() - 5), enddate = Sys.Date()) {
   y.lim <- c(0, max(mthood$`24HourSnowFall_inches`))
   
   g <- ggplot(mthood) +
-    geom_line(aes(DateTimePST, `24HourSnowFall_inches`, color = Location)) +
+    geom_point(aes(DateTimePST, `24HourSnowFall_inches`, color = Location)) +
     ylim(y.lim)
   g
   
 }
 
-#snowfall()
+snowfall()
 
-Precip <- function(startdate = (Sys.Date() - 5), enddate = Sys.Date()) {
+Precip <- function(startdate = (Sys.Date() - 3), enddate = Sys.Date()) {
   url <- paste0('https://www.nwac.us/data-portal/csv/location/mt-hood/sensortype/precipitation/start-date/', startdate, '/', 'end-date/', enddate, '/')  
   mthood <- fread(url)
   
@@ -74,4 +74,4 @@ Precip <- function(startdate = (Sys.Date() - 5), enddate = Sys.Date()) {
   
 }
 
-#Precip()
+Precip()
